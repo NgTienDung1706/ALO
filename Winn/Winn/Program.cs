9e9136d2a62ff62ad057ee5c77bca5682d700cd7
5nganda <quocduck1@gmail.com>
2024-01-26 21:39:33 +0700

    sua output

--- Winn/Winn/Program.cs
+++ Winn/Winn/Program.cs
@@ -15,7 +15,7 @@ namespace Winn
         static void Main()
         {
             Console.WriteLine("Hello from Branch A");
-            Console.WriteLine("Hello from Branch B");
+            Console.WriteLine("Hello from Branch qduc");
         }
     }
 }
